761d6a297919180246aac30ff45c114d098861aa
Marcus Vinicius <buba.sergipe@gmail.com>
2021-12-12 16:03:14 -0300

    Dropdown and FavoriteItem created

---
+++ src/components/Dropdown/index.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import FavoriteItem from '../FavoriteItem';
+
+import { Container, Count, Dropdown, Items, NoItems } from './styles/dropdown';
+import { BsStar } from 'react-icons/bs';
+import { IconContext } from 'react-icons';
+import { useFirebase } from '../../context/firebase';
+
+function DropdownFavorites() {
+  const [open, setOpen] = useState(false);
+
+  const { removeFavoriteFromFirebase, favoritesMovies } = useFirebase();
+
+  return (
+    <>
+      <Container
+        data-testid="dropdown"
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        <IconContext.Provider
+          value={{ style: { color: '#fff', fontSize: 60 } }}
+        >
+          <BsStar />
+        </IconContext.Provider>
+        <Count>{favoritesMovies.length}</Count>
+      </Container>
+      {open && (
+        <Dropdown data-testid="dropdown-list">
+          <Items>
+            {favoritesMovies.length ? (
+              favoritesMovies.map((item) => (
+                <FavoriteItem
+                  key={item.id}
+                  item={item}
+                  remove={() => removeFavoriteFromFirebase(item.id)}
+                />
+              ))
+            ) : (
+              <NoItems>Sem items</NoItems>
+            )}
+          </Items>
+        </Dropdown>
+      )}
+    </>
+  );
+}
+
+export default DropdownFavorites;
