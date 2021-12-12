40b8fa58ede6a3893bc753bc02692b0c9fde6ea2
Marcus Vinicius <buba.sergipe@gmail.com>
2021-12-12 10:21:37 -0300

    Movies service created

---
+++ src/services/movies.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+export const api = axios.create({ baseURL: 'https://api.themoviedb.org/3' });
+
+const key = process.env.REACT_APP_MOVIEDB_API_KEY;
+
+export const searchMovies = async (term) => {
+  const response = await api.get('/search/movie', {
+    params: {
+      api_key: key,
+      query: term,
+      language: 'pt-BR',
+      page: 1,
+    },
+  });
+
+  return response.data.results;
+};
+
+export const getMovieById = async (id) => {
+  try {
+    const response = await api.get(`/movie/${id}`, {
+      params: {
+        api_key: key,
+        language: 'pt-BR',
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error.mesage);
+  }
+};
+
+export const getMovies = async () => {
+  const [nowData, popularData, topRatedData] = await Promise.all([
+    api.get(`/movie/now_playing?api_key=${key}&language=pt-BR&page=1`),
+    api.get(`/movie/popular?api_key=${key}&language=pt-BR&page=1`),
+    api.get(`/movie/top_rated?api_key=${key}&language=pt-BR&page=1`),
+  ]);
+
+  return { nowData, popularData, topRatedData };
+};
