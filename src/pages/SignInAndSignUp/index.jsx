b7332a1a32921864d0b4ed5d2ba391d2f4efd914
Marcus Vinicius <buba.sergipe@gmail.com>
2021-12-13 06:44:27 -0300

    SignIn and SignUp pages

---
+++ src/pages/SignInAndSignUp/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import SignIn from '../../components/SignIn';
+import SignUp from '../../components/SignUp';
+import { SiThemoviedatabase } from 'react-icons/si';
+import { IconContext } from 'react-icons';
+import { Container, SignContainer } from './styles/signin-signup';
+
+function SignInAndSignUp() {
+  return (
+    <Container>
+      <IconContext.Provider value={{ style: { fontSize: 90 } }}>
+        <SiThemoviedatabase />
+      </IconContext.Provider>
+      <SignContainer>
+        <SignIn />
+        <SignUp />
+      </SignContainer>
+    </Container>
+  );
+}
+
+export default SignInAndSignUp;
