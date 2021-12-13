fbb099d26fb2b71dff3af7ce9d6414db895da929
Marcus Vinicius <buba.sergipe@gmail.com>
2021-12-13 07:03:07 -0300

    Input component

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Label, Input } from './styles/input';
+
+// import { Container } from './styles';
+
+function CustomInput({ label = false, ...props }) {
+  return (
+    <>
+      {label && <Label>Email</Label>}
+      <Input {...props} />
+    </>
+  );
+}
+
+export default CustomInput;
